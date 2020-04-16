f4722432339d8d41814f7b171f33b77837c45fbf
RamonaEJSpence <ramonaejspence@gmail.com>
2020-04-16 13:08:43 -0400

    fixed navbar

--- js/script.js
+++ js/script.js
@@ -6,18 +6,24 @@ $( document ).ready(function() {
 
                 $("#about").click(function(){
                     $("#index").fadeOut();
+                    $("#work_scroll").fadeOut();
+                    $("#contact_scroll").fadeOut();
                     $("#about_scroll").fadeIn();
                     $('#about_left').addClass('animated slideInLeft');
                     $('#about_right').addClass('animated slideInRight');
                     });
                 $("#work").click(function(){
                     $("#index").fadeOut();
+                    $("#about_scroll").fadeOut();
+                    $("#contact_scroll").fadeOut();
                     $("#work_scroll").fadeIn();
                     $('#work_left').addClass('animated slideInLeft');
                     $('#work_right').addClass('animated slideInRight');
                     });
                 $("#contact").click(function(){
                     $("#index").fadeOut();
+                    $("#about_scroll").fadeOut();
+                    $("#work_scroll").fadeOut();
                     $("#contact_scroll").fadeIn();
                     $('#contact_left').addClass('animated slideInLeft');
                     $('#contact_right').addClass('animated slideInRight');
